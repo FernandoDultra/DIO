5c07e09b35677099e745c7733f9a8f3a752a7b64
Fernando Dultra dos Santos <101875551+FernandoDultra@users.noreply.github.com>
2024-06-24 11:23:31 -0300

    inserida a opção de abrir o navagador

--- ReconhecimentoDeVoz.py
+++ ReconhecimentoDeVoz.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 import os
 import pyaudio
-print("Estou te ouvindo...")
+print("Inicializando... um momento...")
 
 # função de ouvir e reconhecer a fala
 def ouvir_microfone():
@@ -19,6 +19,9 @@ def ouvir_microfone():
         print("Recognizando...")
         frase = microfone.recognize_google(audio, language='pt-BR')
         print(frase)
+        if "navegador" in frase:
+            os.system('start Chrome.exe')
+            return False
     except sr.UnknownValueError:
         print("Não entendi o que você disse. Tente novamente.")
     except sr.RequestError as e:
